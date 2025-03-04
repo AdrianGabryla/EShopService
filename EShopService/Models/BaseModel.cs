dd4afce89474efe693453504890ec08b3e1e1783
AdrianGabryla <adriangabryla@gmail.com>
2025-03-04 12:15:52 +0100

    Lab1 ex.5

--- EShopService/Models/BaseModel.cs
+++ EShopService/Models/BaseModel.cs
@@ -2,5 +2,14 @@
 {
     public class BaseModel
     {
+        public bool Deleted { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public Guid CreatedBy { get; set; }
+
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        public Guid UpdatedBy { get; set; }
     }
 }
